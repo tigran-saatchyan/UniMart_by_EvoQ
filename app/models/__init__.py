43b4c6c3fdca67f084a89fa89991b04ba808120e
Tigran Saatchyan <mr.saatchyan@gmail.com>
2023-11-12 22:28:43 +0400

    [FEATURE] adds user model - #32 Added user model, User schema, Updated product model by adding owner relationship

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,9 @@
+from app.models.base_model import BaseModel
 from app.models.products import Product
+from app.models.users import User
 
-__all__ = ["Product"]
+__all__ = [
+    "BaseModel",
+    "Product",
+    "User",
+]
--- app/models/products.py
+++ app/models/products.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Float, String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean, Float, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base_model import (
     BaseModel,
@@ -15,6 +15,10 @@ class Product(BaseModel):
     price: Mapped[float] = mapped_column(
         Float(precision=2), server_default="0.00"
     )
+    owner_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("users.id"), nullable=False
+    )
+    owner: Mapped[int] = relationship("User", back_populates="products")
     is_active: Mapped[bool] = mapped_column(
         Boolean, nullable=False, default=False
     )
