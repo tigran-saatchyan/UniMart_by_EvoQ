1df5f435799b2d07eb3539fb9de081b3577b64de
Tigran Saatchyan <mr.saatchyan@gmail.com>
2023-11-12 05:22:43 +0400

    [SETTINGS] setting up project - #2 Prepared factories for database setup, routes setup, Implemented create app factory, cors setup. Set up OpenAPI settings. Created Config Factory class. Added .env.sample file.

---
+++ app/settings/__init__.py
@@ -0,0 +1,3 @@
+from app.settings.config import config
+
+__all__ = [config]
---
+++ app/utils/factories.py
@@ -0,0 +1,63 @@
+from typing import Dict
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
+
+from app.settings import config
+
+
+def create_app() -> FastAPI:
+    application = FastAPI(**config.FASTAPI_SETTINGS)
+    return application
+
+
+def setup_database(application: FastAPI) -> None:
+    # TODO: implement database setup
+    ...
+
+
+def setup_cors(application: FastAPI) -> None:
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=config.CORS_ALLOWED_ORIGINS,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+def setup_routes(application: FastAPI) -> None:
+    # TODO: implement routes registration
+    ...
+
+
+def custom_openapi(application: FastAPI) -> Dict[str, dict]:
+    if application.openapi_schema:
+        return application.openapi_schema
+    openapi_schema = get_openapi(
+        title="UniMart by EvoQ API",
+        version="1.0.0",
+        summary="E-Commerce API solution for developers",
+        description="**UniMart by EvoQ API** is a tool that allows "
+        "developers to integrate e-commerce features "
+        "like product management, inventory control, "
+        "order processing, and customer data management "
+        "into their applications or websites, making it "
+        "easier to run online stores.",
+        routes=application.routes,
+        terms_of_service="",
+        contact={
+            "name": "Tigran Saatchyan (EvoQ) - Backend Developer",
+            "url": "https://github.com/tigran-saatchyan",
+        },
+        license_info={
+            "name": "MIT License",
+            "url": "https://opensource.org/licenses/MIT",
+        },
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    application.openapi_schema = openapi_schema
+    return application.openapi_schema
