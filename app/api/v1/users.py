85dc81748ef088e3343b207225c675d265fd1dbb
Tigran Saatchyan <mr.saatchyan@gmail.com>
2023-11-15 12:02:00 +0400

    [FEATURE] adds user data updating feature - #40 Adds user data updating

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.v1.dependencies import UOWDependency
-from app.schemas.users import UsersSchemaAdd
+from app.schemas.users import UsersSchemaAdd, UsersSchemaEdit
 from app.services.users import UsersService
 
 router = APIRouter(
@@ -15,4 +15,11 @@ async def register_user(user: UsersSchemaAdd, uow: UOWDependency):
     return await UsersService().add(uow, user)
 
 
-# router.post("/login")
+@router.post("/update/{user_id}")
+async def register_user(
+    user_id: int, user: UsersSchemaEdit, uow: UOWDependency
+):
+    result = await UsersService().update(uow, user_id, user)
+    if not result:
+        return {"detail": "Nothing to update"}
+    return {"detail": "User updated"}
